3c1e05c1e268dc69660c02a915a541c172bb92db
sourav-dn <souravdn.dev@gmail.com>
2025-06-14 21:57:57 +0600

    make banner responsive for all devices

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,28 +2,28 @@
 const Banner = () => {
     return (
         <div className="carousel w-full ">
-            <div id="slide1" className="carousel-item relative w-full h-[500px]">
+            <div id="slide1" className="carousel-item relative w-full h-[300px] sm:h-[400px] md:h-[500px]">
                 <img
                     src="https://i.ibb.co/Z64pQYDQ/i1-c.png"
-                    className="w-full" />
+                    className="w-full h-full object-cover" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide2" className="carousel-item relative w-full h-[500px]">
+            <div id="slide2" className="carousel-item relative w-full h-[300px] sm:h-[400px] md:h-[500px]">
                 <img
                     src="https://i.ibb.co/0WPBF8m/i2-c.jpg"
-                    className="w-full" />
+                    className="w-full h-full object-cover" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide3" className="carousel-item relative w-full h-[500px]">
+            <div id="slide3" className="carousel-item relative w-full h-[300px] sm:h-[400px] md:h-[500px]">
                 <img
                     src="https://i.ibb.co/G4fDv8tk/i3-c.jpg"
-                    className="w-full" />
+                    className="w-full h-full object-cover" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
