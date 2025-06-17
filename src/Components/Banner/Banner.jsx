689bfabcc2992c0971ca133e1e850e140127afb5
sourav-dn <souravdn.dev@gmail.com>
2025-06-17 16:47:15 +0600

    modify banner image height

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,16 +2,16 @@
 const Banner = () => {
     return (
         <div className="carousel w-full ">
-            <div id="slide1" className="carousel-item relative w-full h-[300px] sm:h-[400px] md:h-[500px]">
+            <div id="slide1" className="carousel-item relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px]">
                 <img
-                    src="https://i.ibb.co/WWWm2Qvw/text-behind-photo-599x336.png"
+                    src="https://i.ibb.co/6JWccDXj/pet1.jpg"
                     className="w-full h-full object-cover" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide2" className="carousel-item relative w-full h-[300px] sm:h-[400px] md:h-[500px]">
+            <div id="slide2" className="carousel-item relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px]">
                 <img
                     src="https://i.ibb.co/0WPBF8m/i2-c.jpg"
                     className="w-full h-full object-cover" />
@@ -20,9 +20,9 @@ const Banner = () => {
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide3" className="carousel-item relative w-full h-[300px] sm:h-[400px] md:h-[500px]">
+            <div id="slide3" className="carousel-item relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px]">
                 <img
-                    src="https://i.ibb.co/8nJmL3rT/text-behind-photo-612x408.png"
+                    src="https://i.ibb.co/0jz0YSN6/garden2-edit.jpg"
                     className="w-full h-full object-cover" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide2" className="btn btn-circle">❮</a>
